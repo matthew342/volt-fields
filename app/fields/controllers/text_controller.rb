0ecc4ddef4c93290da59d04da81c36d485c75fc1
Matt Hale <matt.hale.0@gmail.com>
2015-08-22 09:42:40 -0700

    add labels for text fields

--- app/fields/controllers/text_controller.rb
+++ app/fields/controllers/text_controller.rb
@@ -2,5 +2,43 @@ require 'fields/controllers/main_controller'
 
 module Fields
   class TextController < MainController
+    reactive_accessor :toggled
+
+    def input_class_string
+      class_string = ""
+      if attrs.prepend_class
+        class_string += 'input-group'
+      else
+        class_string += 'form-group'
+      end
+      if attrs.label && attrs.label_type == 'float'
+        class_string += ' fg-float'
+      end
+      if errors
+        class_string += ' has-error'
+      elsif marked
+        class_string += ' has-success'
+      end
+      class_string += ' has-feedback'
+      class_string
+    end
+
+    def toggle_class
+      value = model_inst.send(@field_name)
+      unless value.nil? || value.blank?
+        'fg-toggled'
+      else
+        toggled
+      end
+    end
+
+    def toggle
+      self.toggled = 'fg-toggled'
+    end
+
+    def blur
+      self.toggled = ''
+      super
+    end
   end
 end
